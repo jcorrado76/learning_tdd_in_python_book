018ed0ba4aba0a3544c2b9ddf9a903bfdda42e1f
Joseph Corrado <josephcorrado7@gmail.com>
2021-11-11 15:34:37 -0500

    Finished division feature and some minor refactoring. We also implemented equality for our Money class.

--- 2_chapter/README.md
+++ 2_chapter/README.md
@@ -10,3 +10,15 @@ We've copied our list of features from chapter 1 so we can continue working on t
 
 
 We're going to start with working on the second feature in this chapter. 
+
+We are also going to work on the third feature in this chapter, division.
+
+When comparing equality in our tests, we can toy with implementing the `__eq__` method for our classes.
+
+By default, equality in python is done by reference: two objects are equal if they point to the same object.
+This is a very strict definition of equality - it means that an object is **only** equal to itself, not any other object,
+even if they have the same state. 
+
+Equality is determined by reference, not by value by default.
+
+So, we just need to explicitly override the `__eq__` method.
--- 2_chapter/test_money.py
+++ 2_chapter/test_money.py
@@ -1,19 +1,39 @@
 import unittest
 
 
-class Dollar:
-    def __init__(self, amount: float):
+class Money:
+    def __init__(self, amount: float, currency: str):
         self.amount = amount
+        self.currency = currency
 
     def times(self, multiplier: float):
-        return Dollar(self.amount * multiplier)
+        return Money(self.amount * multiplier, self.currency)
+
+    def divide(self, divisor: float):
+        return Money(self.amount / divisor, self.currency)
+
+    def __eq__(self, other):
+        return self.amount == other.amount and self.currency == other.currency
 
 
 class TestMoney(unittest.TestCase):
-    def testMultiplication(self):
-        fiver = Dollar(5)
-        tenner = fiver.times(2)
-        self.assertEqual(10, tenner.amount)
+    def testMultiplicationInDollars(self):
+        fiveDollars = Money(5, "USD")
+        tenDollars = Money(10, "USD")
+        self.assertEqual(tenDollars, fiveDollars.times(2))
+
+    def testMultiplicationInEuros(self):
+        ten_euros = Money(10, "EUR")
+        twenty_euros = ten_euros.times(2)
+        self.assertEqual(twenty_euros, Money(20, "EUR"))
+
+    def testDivision(self):
+        originalMoney = Money(4002, "KRW")
+        actualMoneyAfterDivision = originalMoney.divide(4)
+        expectedMoneyAfterDivision = Money(1000.5, "KRW")
+
+        self.assertEqual(expectedMoneyAfterDivision.amount, actualMoneyAfterDivision.amount)
+        self.assertEqual(expectedMoneyAfterDivision.currency, actualMoneyAfterDivision.currency)
 
 
 if __name__ == "__main__":
