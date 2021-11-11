6fcb852b14d4258f620067ec292bdc451feb955d
Joseph Corrado <josephcorrado7@gmail.com>
2021-11-11 16:46:06 -0500

    Added support to create a portfolio and add moneys of the same currency together.

--- 3_chapter/test_money.py
+++ 3_chapter/test_money.py
@@ -16,6 +16,20 @@ class Money:
         return self.amount == other.amount and self.currency == other.currency
 
 
+class Portfolio:
+    def __init__(self):
+        self.moneys = []
+
+    def add(self, *moneys):
+        self.moneys.extend(moneys)
+
+    def evaluate(self, currency):
+        total = 0
+        for money in self.moneys:
+            total += money.amount
+        return Money(total, currency)
+
+
 class TestMoney(unittest.TestCase):
     def testMultiplicationInDollars(self):
         fiveDollars = Money(5, "USD")
@@ -34,6 +48,14 @@ class TestMoney(unittest.TestCase):
 
         self.assertEqual(expectedMoneyAfterDivision.amount, actualMoneyAfterDivision.amount)
         self.assertEqual(expectedMoneyAfterDivision.currency, actualMoneyAfterDivision.currency)
+
+    def testAddition(self):
+        fiveDollars = Money(5, "USD")
+        tenDollars = Money(10, "USD")
+        fifteenDollars = Money(15, "USD")
+        portfolio = Portfolio()
+        portfolio.add(fiveDollars, tenDollars)
+        self.assertEqual(fifteenDollars, portfolio.evaluate("USD"))
 
 
 if __name__ == "__main__":
